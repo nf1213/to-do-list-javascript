edac7d2530a1a01a1779b281ab6ccb30dbba82c8
Nicole <nfelch121394@gmail.com>
2015-01-30 13:09:35 -0500

    Add items

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,19 @@
+document.forms['todo'].add.addEventListener('click', addItem);
 
+function addItem() {
+  var item = document.getElementById('new');
+  if(item.value !== "") {
+    var list = document.getElementById('list');
+    var li = document.createElement('li');
+    li.innerHTML = item.value;
+    list.appendChild(li);
+    item.value = "";
+  }
+}
+
+function enterAdd(e) {
+  if (e.keyCode == 13 || e.which == 13) {
+    addItem();
+    return false;
+  }
+}
